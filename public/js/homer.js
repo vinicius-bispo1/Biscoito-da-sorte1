b4f1d3b415d51ef1286a3dda14f6c7c660237c30
vinicius bispo <vini.bispo12@hotmail.com>
2022-06-09 14:29:09 -0300

    'master'

--- public/js/homer.js
+++ public/js/homer.js
@@ -1,16 +1,16 @@
-//solicitando a div que iremos utilizar para colocar as frases
+
 let divSortes = document.querySelector('.card-body');
-//solicitando o botao que iremos fazer a função de clique
+
 let button = document.querySelector('.btn');
-//adicionamos o evento do botão que ao clicar chamara nossa função
+
 button.addEventListener('click', sortesAleatoria);
 
-//criamos a função que mostrará as frases aleatórias
+
 function sortesAleatoria() {
 
-    //criando numero randomico que servirá de index do nosso array de frases
-    let i = Math.floor(Math.random() * ( 356 - 0 + 1) + 0)
-    //adiciona o texto da frase na div criada
+
+    let i = Math.round(Math.random() * 5);
+    
     divSortes.textContent = sortes[i];
 
 };
